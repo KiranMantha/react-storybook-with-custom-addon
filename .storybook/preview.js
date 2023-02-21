e9d6c54cc036d820bd3f7c577b96253e9354f66b
KiranMantha <kiranv.mantha@gmail.com>
2023-02-21 16:42:54 +0530

    added storybook with custom toolbar addon

---
+++ .storybook/preview.js
@@ -0,0 +1,28 @@
+import '../src/theme.css';
+import {useTheme} from './useTheme';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Toggle theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+      showName: true,
+      dynamicTitle: true
+    }
+  }
+}
+
+export const decorators = [useTheme];
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-storybook-with-custom-toolbar-addon
-an example react storybook on implementing a custom toolbar addon
+an example react storybook application on implementing a custom toolbar addon
+
+## USeful links
+1. [setting up storybook for react app](https://dev.to/sanchithasr/how-to-implement-storybook-in-react-typescript-material-ui-application-4om7)
+
+2. [creating custom toolbar addon for storybook](https://dev.to/kiranmantha/how-to-create-a-custom-toolbar-addon-for-storybook-5e2e)
